7e6c8bababaf97c03c740dbcd4952767f497496e
leo <68909774+leo15dev@users.noreply.github.com>
2026-07-02 14:01:44 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import os
-import logging
+import sys
 import signal
-from geoip2influx import LogParser, configure_logging
+import logging
 
-from dotenv import load_dotenv
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
-def handle_sigterm(signum, frame):
-    logger = logging.getLogger("g2i")
-    logger.info("Received SIGTERM. Exiting GeoIP2Influx.")
+from geoip2influx.logparser import LogParser
+
+parser = None
+
+def handle_shutdown(signum, frame):
+    global parser
+    logger = logging.getLogger("geoip2influx")
+    logger.info("A termination signal (SIGTERM/SIGINT) has been received, instructing the Parser to gracefully shut down and flush the remaining logs....")
+    if parser:
+        parser.running = False
+    else:
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, handle_shutdown)
+    signal.signal(signal.SIGTERM, handle_shutdown)
+
+    logger = logging.getLogger("geoip2influx")
+    logger.info("Start GeoIP2Influx ...")
+
     try:
-        parser.client.influx.close()
-    except Exception:
-        logger.exception("Error closing InfluxDB client.")
-    logger.info("Parsed %d log line(s).", parser.parsed_lines)
-    exit(0)
-
-if __name__ == "__main__":
-    try:
-        configure_logging(os.getenv("GEOIP2INFLUX_LOG_LEVEL", "debug"))
-        signal.signal(signal.SIGTERM, handle_sigterm)
-        logger = logging.getLogger("g2i")
-        logger.info("Starting GeoIP2Influx.")
         parser = LogParser()
         parser.run()
     except KeyboardInterrupt:
-        logger.info("Exiting GeoIP2Influx.")
-        logger.info("Parsed %d log line(s).", parser.parsed_lines)
-        exit(0)
-    except Exception:
-        logger.exception("Error running parser.")
-        exit(1)
+        logger.info("User manually interrupts, safely exits")
+        sys.exit(0)
+    except Exception as e:
+        logger.exception(f"An unexpected error occurred during the GeoIP2Influx runtime.An unexpected error occurred during the GeoIP2Influx runtime.: {e}")
+        sys.exit(1)
